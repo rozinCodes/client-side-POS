30ab946866c0039986a53a533104e9f3e7487666
rozinhasan@gmail.com <rozinhasan@gmail.com>
2021-11-13 09:13:01 +0600

    initial commit

--- App.js
+++ App.js
@@ -1,21 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import Home from './screens/home';
+import Cart from './screens/cart';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<NavigationContainer>
+			<Tab.Navigator
+				screenOptions={({ route }) => ({
+					tabBarIcon: ({ focused, color, size }) => {
+						let iconName;
+
+						if (route.name === 'Home') {
+							iconName = focused ? 'ios-information-circle' : 'ios-information-circle-outline';
+						} else if (route.name === 'Settings') {
+							iconName = focused ? 'ios-list-box' : 'ios-list';
+						}
+
+						return <Ionicons name={iconName} size={size} color={color} />;
+					},
+					tabBarActiveTintColor: 'tomato',
+					tabBarInactiveTintColor: 'gray'
+				})}
+			>
+				<Tab.Screen name="Home" component={Home} />
+				<Tab.Screen name="Cart" component={Cart} />
+			</Tab.Navigator>
+		</NavigationContainer>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center'
+	}
 });
---
+++ screens/cart.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const Cart = () => {
+    return (
+        <View>
+            <Text style = {{textAlign: 'center'}}>Cart Screen</Text>
+        </View>
+    )
+}
+
+export default Cart
