3dc4fbda2612d1a88b9af66dc6b2df1508d2d438
rozinhasan@gmail.com <rozinhasan@gmail.com>
2021-11-13 09:21:33 +0600

    presets

---
+++ presets/colors.js
@@ -0,0 +1,14 @@
+export const colors = {
+    black: '#070724',
+    white: '#fff',
+    darkgrey: '#38384f',
+    grey: '#838391',
+    lightgreen: '#419ebb',
+    orange: '#eda249',
+    purple: '#6f2ed6',
+    darkorange: '#d14c32',
+    red: '#d83134',
+    lightred: '#cd2150',
+    green: '#1ec2a4',
+    blue: '#2d68f0'
+}
